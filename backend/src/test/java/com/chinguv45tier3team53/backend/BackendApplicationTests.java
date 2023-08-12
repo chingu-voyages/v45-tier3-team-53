aa916ef61096219f32171f0c22ed2a96abd75364
Chris Yoo <woyoo@umich.edu>
2023-08-11 22:41:39 -0400

    start backend with spring boot

---
+++ backend/src/test/java/com/chinguv45tier3team53/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinguv45tier3team53.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
