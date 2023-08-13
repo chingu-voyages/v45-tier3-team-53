de292742ab739c05a9d7eb6e174afe0c7a30a9ba
Chris Yoo <woyoo@umich.edu>
2023-08-12 22:51:19 -0400

    start backend with spring boot project

---
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/TripPlannerRestApplication.java
@@ -0,0 +1,13 @@
+package com.chinguv45tier3team53.tripplannerrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlannerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPlannerRestApplication.class, args);
+	}
+
+}
