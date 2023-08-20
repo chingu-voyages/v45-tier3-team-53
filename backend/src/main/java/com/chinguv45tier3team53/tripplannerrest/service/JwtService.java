3a535df68c02f93b4d7500ecdc7f2ae6c8279d26
Chris Yoo <woyoo@umich.edu>
2023-08-20 02:52:22 -0400

    create secret key

--- backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/service/JwtService.java
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/service/JwtService.java
@@ -2,11 +2,17 @@ package com.chinguv45tier3team53.tripplannerrest.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
-
+    @Value("${SECRET_KEY}")
+    private static final String SECRET_KEY;
     public String extractUserEmail(String token) {
         return null;
     }
@@ -18,5 +24,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
