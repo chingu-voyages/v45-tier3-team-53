3052ba37a3ba4c98de9e2fec929f16ef97cb0c3d
Chris Yoo <woyoo@umich.edu>
2023-08-20 16:54:32 -0400

    white list calls to authentication

--- backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/security/SecurityConfiguration.java
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/security/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers()
+                .requestMatchers("/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
