2996492bdc13ef2a0b9edc5e45ed517d4684676c
Chris Yoo <woyoo@umich.edu>
2023-08-20 11:33:37 -0400

    create a bean for UserDetailsService

---
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package com.chinguv45tier3team53.tripplannerrest;
+
+import com.chinguv45tier3team53.tripplannerrest.dao.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
