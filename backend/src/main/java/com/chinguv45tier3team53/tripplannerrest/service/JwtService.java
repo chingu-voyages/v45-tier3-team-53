36f3d1cd5d43e2bc5df4556270d554f8ce55fc31
Chris Yoo <woyoo@umich.edu>
2023-08-20 03:05:23 -0400

    create a method for extracting a single claim

--- backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/service/JwtService.java
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/service/JwtService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -24,6 +25,11 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Key getSignInKey() {
