4cf06afea917d34accc423f8ea39518b95d43f2b
Chris Yoo <woyoo@umich.edu>
2023-08-20 12:39:29 -0400

    create RegisterRequest class

---
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/controller/auth/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.chinguv45tier3team53.tripplannerrest.controller.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
