35f22d0c879328ead050a69efe37eec843e92528
Chris Yoo <woyoo@umich.edu>
2023-08-20 16:47:21 -0400

    implement authenticate method in AuthenticationService class

--- backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/service/AuthenticationService.java
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/service/AuthenticationService.java
@@ -7,6 +7,8 @@ import com.chinguv45tier3team53.tripplannerrest.dao.UserRepository;
 import com.chinguv45tier3team53.tripplannerrest.entity.Role;
 import com.chinguv45tier3team53.tripplannerrest.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,7 @@ public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
                 .firstname(request.getFirstname())
@@ -32,5 +35,17 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
