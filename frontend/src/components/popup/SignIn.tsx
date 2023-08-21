ef616a389aaf628941688acf5e4f15364f0d3821
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2023-08-20 17:21:44 -0700

    created SignIn functionality

--- frontend/src/components/popup/SignIn.tsx
+++ frontend/src/components/popup/SignIn.tsx
@@ -1,38 +1,82 @@
-import { useReactiveVar } from "@apollo/client";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { user, userState } from "../../GlobalState";
-import axios from "axios";
+import { FormikProps, Form, Field, withFormik, FormikErrors, Formik, FormikHelpers } from "formik";
+import { userState } from '../../GlobalState';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-const SignIn=()=> {
 
-    const [userEmail, SetUserEmail] = useState<string>();
-    const [userPassword, setUserPassword] = useState<string>();
-    const [ error, setError] = useState<string>();
+interface SignInFormValues {
+    email: string
+    password: string
+}
+
+const emailVaild = (email: string) => {
+    const isValid = email ? /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(email) : false;
+    const isEmpty = email?.length === 0;
+
+    return isValid && !isEmpty;
+}
+
+const SignInPopUp = () => {
     const navigate = useNavigate();
-    const user = useReactiveVar<user|undefined>(userState);
 
-    const onLoginHandler = (e: React.MouseEvent<HTMLElement>) => {
-        e.preventDefault();
+    const onSubmit = (values: SignInFormValues, { setErrors, setSubmitting }: FormikHelpers<SignInFormValues>) => {
+
+        console.log(values);
+    
         axios
-            .post(`http://localhost:5173/tripplanner/login`,{
-                email: userEmail,
-                password: userPassword
-            })
-            .then((res) =>{
-                userState(res.data.user);
+            .post(`http://localhost:8080/auth/authenticate`, values)
+            .then((res) => {
+                userState(res.data);
                 navigate(`/trips`)
+                setSubmitting(false);
             })
-            .catch((err)=>{
-                setError("Your Email or Password is incorrect.");
+            .catch((err) => {
+                setErrors({
+                    // TODO: get error from API call
+                })
+                setSubmitting(false);
             })
+        return;
+    }
+
+    const validate = (values: SignInFormValues) => {
+        let errors: FormikErrors<SignInFormValues> = {};
+        if (!values.email) {
+            errors.email = 'Required';
+        } else if (!emailVaild(values.email)) {
+            errors.email = 'Invalid email address';
+        }
+        return errors;
     }
 
     return (
         <div>
-            
+            <h1>SignIn</h1>
+            <Formik 
+                initialValues={{
+                    email: "",
+                    password: ""
+                }}
+                onSubmit={onSubmit}
+                validate={validate}
+            >
+                {({errors, touched, isSubmitting}: FormikProps<SignInFormValues>)=>(
+                <Form>
+                    
+                    <label htmlFor="email">Email</label>
+                    <Field className='' type="email" name="email" id="email"/>
+                    {touched.email && errors.email && <div>{errors.email}</div>}
+
+                    <label htmlFor="password">Password</label>
+                    <Field type="password" name="password" />
+                    {touched.password && errors.password && <div>{errors.password}</div>}
+
+                    <button type="submit" disabled={isSubmitting} >Submit</button>
+                </Form> 
+                )}
+            </Formik>
         </div>
     )
 };
 
-export default SignIn;
+export default SignInPopUp;
