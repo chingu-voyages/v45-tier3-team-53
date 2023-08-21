79a8adcccc6245cb1698879f8229198e35616116
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2023-08-20 17:22:01 -0700

    created SignUp Functionality

--- frontend/src/components/popup/SignUp.tsx
+++ frontend/src/components/popup/SignUp.tsx
@@ -1,85 +1,118 @@
-import { useReactiveVar } from "@apollo/client";
-import { useState, useMemo } from "react";
-import { user, userState } from "../../GlobalState";
+import { userState } from "../../GlobalState";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { Field, Form, Formik, FormikErrors, FormikHelpers, FormikProps } from "formik";
 
-type newUser = {
-        firstName?: string,
-        lastName?: string,
-        email?: string,
-        password?: string,
+type newUserFormValues = {
+        firstName: string,
+        lastName: string,
+        email: string,
+        password: string,
+        confirmedPassword: string
     }
 
-type userFormError = {
-    email : boolean,
-    body : String
+type updatedNewUser = {
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
 }
 
+const emailVaild = (email: string) => {
+    const isValid = email ? /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(email) : false;
+    const isEmpty = email?.length === 0;
 
-const SignUp = () => {
-    const [error, setError] = useState<userFormError>({});
-    const user = useReactiveVar<user|undefined>(userState);
-    const [confirmedPassword, setConfirmedPassword] = useState<string>("");
-    const [newUser, setNewUser] = useState<newUser>({});
+    return isValid && !isEmpty;
+}
+
+const SignUpPopUp =()=>{
     const navigate = useNavigate();
 
-    const passwordMatch = (confirmedPassword : string, newUser: newUser ) => {
-        if(!confirmedPassword || !newUser.password){
-            return true;
+    const onSubmit = (values: newUserFormValues, { setErrors, setSubmitting }: FormikHelpers<newUserFormValues>) => {
+        console.log(values);
+
+        const updatedNewUser: updatedNewUser= {
+            firstName: values.firstName,
+            lastName: values.lastName,
+            email: values.email,
+            password: values.password
         }
-        else if( confirmedPassword = newUser.password){
-            return true;
-        }
-        return false;
-    };
-
-    const emailVaild = useMemo(()=>{
-        const isValid = newUser && newUser.email ? /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(newUser.email) : false;
-        const isEmpty = newUser?.email?.length === 0;
-
-        return {
-            isValid,
-            isEmpty
-        };
-    }, [newUser?.email])
-
-    const newUserSubmitHandler = (e: React.MouseEvent<HTMLElement>) => {
-        e.preventDefault;
-        const passwordsMatch = passwordMatch(confirmedPassword, newUser)
-        if(!emailVaild.isValid || emailVaild.isEmpty || !passwordsMatch){
-            setError({
-                email : true,
-                body: "Your email is invalid or passwords do not match."
-            
-            })
-            return;
-        }
-
+        
         axios
-            .post(`http://localhost:5173/tripplanner/signup`)
+            .post(`http://localhost:8080/auth/register`, updatedNewUser)
             .then((res) => {
                 userState(res.data);
                 navigate(`/trips`)
+                setSubmitting(false);
             })
             .catch((err) => {
-                setError({
-                    email: true,
-                    // TODO: replace with error given from API once known
-                    body: "There was an issue with submitting."
+                setErrors({
+                    // TODO: get error from API call
                 })
+                setSubmitting(false);
             })
+        return;
     }
 
-    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const newUserForm : newUser = {...newUser};
-        const fieldName: "firstName" | "lastName" | "email" | "password" = e.target.name as "firstName" | "lastName" | "email" | "password"
-        newUserForm[fieldName] = e.target.value;
-    }
+        const validate = (values: newUserFormValues) => {
+            let errors: FormikErrors<newUserFormValues> = {};
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!emailVaild(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            if(!values.password){
+                errors.password = 'Required';
+            }
+            if(!values.confirmedPassword){
+                errors.confirmedPassword = 'Required';
+            } else if(values.password && values.password !== values.confirmedPassword) {
+                errors.password = "Passwords don't match.";
+            }
+            return errors;
+        }
 
     return (
         <div>
-            
+            <h1>SignUp</h1>
+            <Formik 
+                initialValues={{
+                    firstName: "",
+                    lastName: "",
+                    email: "",
+                    password: "",
+                    confirmedPassword: ""
+                }}
+                onSubmit={onSubmit}
+                validate={validate}
+            >
+                {({errors, touched, isSubmitting}: FormikProps<newUserFormValues>)=>(
+                <Form>
+
+                    <label htmlFor="firstName">First Name</label>
+                    <Field className='' name="firstName" id="firstName"/>
+                    
+                    <label htmlFor="lastName">Last Name</label>
+                    <Field className='' name="lastName" id="lastName"/>
+
+                    <label htmlFor="email">Email</label>
+                    <Field className='' type="email" name="email" id="email"/>
+                    {touched.email && errors.email && <div>{errors.email}</div>}
+
+                    <label htmlFor="password">Password</label>
+                    <Field type="password" name="password" />
+                    {touched.password && errors.password && <div>{errors.password}</div>}
+
+                    <label htmlFor="confirmedPassword">Confirm Password</label>
+                    <Field type="password" name="confirmedPassword" />
+                    {touched.confirmedPassword && errors.confirmedPassword && <div>{errors.confirmedPassword}</div>}
+
+                    <button type="submit" disabled={isSubmitting} >Submit</button>
+                </Form> 
+                )}
+            </Formik>
         </div>
     )
 }
+
+export default SignUpPopUp;
