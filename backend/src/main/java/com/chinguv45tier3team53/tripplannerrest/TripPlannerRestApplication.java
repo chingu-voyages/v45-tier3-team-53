18a19f99b71d9fcfc79bbf20df9cc6ea60fde480
Angela Lee <angelalee@Angelas-iMac.local>
2023-08-22 22:07:00 -0500

    clean up

--- backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/TripPlannerRestApplication.java
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/TripPlannerRestApplication.java
@@ -8,31 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TripPlannerRestApplication implements CommandLineRunner {
+public class TripPlannerRestApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TripPlannerRestApplication.class, args);
 	}
-	@Autowired
-	TripRepository tripRepository;
-	public void run(String... args) throws Exception{
-		try {
-			Trip trip= Trip.builder()
-					.user_id(100L).hotel_id(100L).title("Summer in Tokyo").detail("A family trip to Tokyo Japan").build();
-			tripRepository.save(trip);
-			Trip trip1= Trip.builder()
-					.user_id(200L).hotel_id(200L).title("Autumn in New York").detail("A family trip to New York city in USA").build();
-			tripRepository.save(trip1);
-			Trip trip2= Trip.builder()
-					.user_id(100L).hotel_id(100L).title("Winter in Austin").detail("A family trip to Austin Texas").build();
-			tripRepository.save(trip2);
-			Trip trip3= Trip.builder()
-					.user_id(100L).hotel_id(100L).title("Spring in Budapest").detail("A family trip to Budapest Hungary").build();
-			tripRepository.save(trip3);
 
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
 
 }
