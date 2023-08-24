b108329cc6d4cdcf96c335307d98310e36f57ab8
Chris Yoo <woyoo@umich.edu>
2023-08-24 17:51:46 -0400

    implement logout service

--- backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/service/LogoutService.java
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/service/LogoutService.java
@@ -1,5 +1,6 @@
 package com.chinguv45tier3team53.tripplannerrest.service;
 
+import com.chinguv45tier3team53.tripplannerrest.dao.TokenRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -10,11 +11,22 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
-
+    private final TokenRepository tokenRepository;
     @Override
     public void logout(HttpServletRequest request,
                        HttpServletResponse response,
                        Authentication authentication) {
-
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        jwt = authHeader.substring(7);
+        var storedToken = tokenRepository.findByToken(jwt).orElse(null);
+        if(storedToken != null) {
+            storedToken.setExpired(true);
+            storedToken.setRevoked(true);
+            tokenRepository.save(storedToken);
+        }
     }
 }
