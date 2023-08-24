7efe6a6d9981bcc43ac3788394cd4bde77c7f089
Chris Yoo <woyoo@umich.edu>
2023-08-24 17:38:09 -0400

    create logout service

---
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/service/LogoutService.java
@@ -0,0 +1,20 @@
+package com.chinguv45tier3team53.tripplannerrest.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler {
+
+    @Override
+    public void logout(HttpServletRequest request,
+                       HttpServletResponse response,
+                       Authentication authentication) {
+
+    }
+}
