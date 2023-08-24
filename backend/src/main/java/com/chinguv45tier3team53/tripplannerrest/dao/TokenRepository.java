aa222e8b2395330a28f74b39a8b255841e4ac46e
Chris Yoo <woyoo@umich.edu>
2023-08-24 15:38:30 -0400

    revoke all tokens

--- backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/dao/TokenRepository.java
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/dao/TokenRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface TokenRepository extends JpaRepository<Token, Integer> {
     @Query("""
         select t from Token t inner join User u on t.user.id = u.id
-        where u.id = :userId and (t.expired = false or t.revoked false)
+        where u.id = :userId and (t.expired = false or t.revoked = false)
     """)
     List<Token> findAllValidTokensByUser(Integer userId);
 
