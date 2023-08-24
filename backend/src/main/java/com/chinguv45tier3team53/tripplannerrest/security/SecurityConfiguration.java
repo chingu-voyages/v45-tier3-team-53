757f48d83482aabb8b0f3c008372106fafa91e7e
Chris Yoo <woyoo@umich.edu>
2023-08-24 17:13:50 -0400

    add logout to SecurityConfiguration class

--- backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/security/SecurityConfiguration.java
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -39,7 +40,14 @@ public class SecurityConfiguration {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout()
+                .logoutUrl("/auth/logout")
+                .addLogoutHandler(null)
+                .logoutSuccessHandler(
+                        (request, response, authentication) ->
+                        SecurityContextHolder.clearContext()
+                );
 
         return http.build();
     }
