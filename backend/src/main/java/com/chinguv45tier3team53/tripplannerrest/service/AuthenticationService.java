61a253a7d1f9e1f867ccccbb99b5def90b8f7641
Chris Yoo <woyoo@umich.edu>
2023-08-24 12:38:54 -0400

    save the generated token

--- backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/service/AuthenticationService.java
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/service/AuthenticationService.java
@@ -3,8 +3,11 @@ package com.chinguv45tier3team53.tripplannerrest.service;
 import com.chinguv45tier3team53.tripplannerrest.controller.auth.AuthenticationRequest;
 import com.chinguv45tier3team53.tripplannerrest.controller.auth.AuthenticationResponse;
 import com.chinguv45tier3team53.tripplannerrest.controller.auth.RegisterRequest;
+import com.chinguv45tier3team53.tripplannerrest.dao.TokenRepository;
 import com.chinguv45tier3team53.tripplannerrest.dao.UserRepository;
 import com.chinguv45tier3team53.tripplannerrest.entity.Role;
+import com.chinguv45tier3team53.tripplannerrest.entity.Token;
+import com.chinguv45tier3team53.tripplannerrest.entity.TokenType;
 import com.chinguv45tier3team53.tripplannerrest.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthenticationService {
     private final UserRepository repository;
+    private final TokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
@@ -27,11 +31,23 @@ public class AuthenticationService {
                 .password(passwordEncoder.encode(request.getPassword()))
                 .role(Role.USER)
                 .build();
-        repository.save(user);
+        var savedUser = repository.save(user);
         var jwtToken = jwtService.generateToken(user);
+        saveUserToken(savedUser, jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
+    }
+
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .revoked(false)
+                .expired(false)
+                .build();
+        tokenRepository.save(token);
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
@@ -44,6 +60,7 @@ public class AuthenticationService {
         var user = repository.findByEmail(request.getEmail())
                 .orElseThrow();
         var jwtToken = jwtService.generateToken(user);
+        saveUserToken(user, jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
