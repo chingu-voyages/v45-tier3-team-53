99cdc6bcf5e2f2e9967f5bd4978e040338e1f1e1
Shawn Koong <shawnkoong@gmail.com>
2023-08-27 23:39:38 -0700

    removing globalState & formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
-import { Libraries, useJsApiLoader } from "@react-google-maps/api";
-import { NewTripForm } from "./components/NewTripForm.tsx";
+import {Libraries, useJsApiLoader} from "@react-google-maps/api";
+import {NewTripForm} from "./components/NewTripForm.tsx";
 
 const libraries: Libraries = ["places", "maps"];
 
 function App() {
-  const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: import.meta.env.VITE_GOOGLE_API_KEY,
-    libraries: libraries,
-  });
+    const {isLoaded} = useJsApiLoader({
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_API_KEY,
+        libraries: libraries,
+    });
 
-  localStorage.setItem("isLoaded", String(isLoaded));
+    localStorage.setItem("isLoaded", String(isLoaded));
 
-  return <NewTripForm />;
+    return <NewTripForm/>;
 }
 
 export default App
--- frontend/src/GlobalState.tsx
+++
@@ -1,8 +0,0 @@
-import {makeVar} from "@apollo/client";
-
-//placeholder until user model is made in Java
-type user = {
-    userId: String
-}
-
-export const userState = makeVar <user|undefined>(undefined);
