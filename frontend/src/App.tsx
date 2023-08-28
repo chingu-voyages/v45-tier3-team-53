c50e35bc4ce265b0cf7040e952959f26dcfdcda7
Shawn Koong <shawnkoong@gmail.com>
2023-08-27 20:09:50 -0700

    loading google api in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,16 @@
 import {TestContainer} from "./components/TestContainer.tsx";
+import {Libraries, useJsApiLoader} from "@react-google-maps/api";
+
+const libraries: Libraries = ["places", "maps"];
 
 function App() {
+
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_API_KEY,
+    libraries: libraries,
+  })
+
+  localStorage.setItem("isLoaded", String(isLoaded))
 
   return (
       <TestContainer />
