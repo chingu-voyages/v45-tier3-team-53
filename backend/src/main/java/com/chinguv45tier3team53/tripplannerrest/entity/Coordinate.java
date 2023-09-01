153b90217070cd7c5dd4bbd3b095e9074f0eb553
Angela Lee <angelalee@Angelas-iMac.local>
2023-08-31 23:00:52 -0500

    add coordinate class

---
+++ backend/src/main/java/com/chinguv45tier3team53/tripplannerrest/entity/Coordinate.java
@@ -0,0 +1,12 @@
+package com.chinguv45tier3team53.tripplannerrest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Coordinate {
+    private double latitude;
+    private double longitude;}
