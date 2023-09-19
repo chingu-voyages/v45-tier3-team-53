da89957ea6ff33164666e2e3b0cc1a6ef8ce4eaa
Shawn Koong <shawnkoong@gmail.com>
2023-09-18 20:30:35 -0700

    title card

--- frontend/src/components/Itinerary.tsx
+++ frontend/src/components/Itinerary.tsx
@@ -10,38 +10,49 @@ const tabs = [
 
 const TitleCard = () => {
   const destination = useAppSelector((state) => state.trip.destination.name);
-  return <div>Trip to {destination}</div>;
+  const from = useAppSelector(state => state.trip.from);
+  const to = useAppSelector(state => state.trip.to);
+  return (
+    <>
+      <div className="text-5xl font-semibold text-center">Trip to {destination}</div>
+      <div className="text-center mt-1 mb-2">
+          {from}-{to}
+      </div>
+    </>
+  );
 };
 
 export const Itinerary = () => {
   return (
     <>
       <TitleCard />
-      <Tab.Group>
-        <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
-          {tabs.map((title) => (
-            <Tab
-              key={title}
-              className={({ selected }) =>
-                `w-full rounded-lg py-2 text-sm font-medium text-blue-600 
+      <div className="p-1">
+        <Tab.Group>
+          <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+            {tabs.map((title) => (
+              <Tab
+                key={title}
+                className={({ selected }) =>
+                  `w-full rounded-lg py-2 text-sm font-medium text-blue-600 
                 ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 ${
                   selected
                     ? `bg-white shadow`
                     : `hover:bg-white/[0.25] hover:text-black`
                 }`
-              }
-            >
-              {title}
-            </Tab>
-          ))}
-        </Tab.List>
-        <Tab.Panels>
-          <Tab.Panel>hotels and lodging</Tab.Panel>
-          <Tab.Panel>food</Tab.Panel>
-          <Tab.Panel>transportation</Tab.Panel>
-          <Tab.Panel>places to visit</Tab.Panel>
-        </Tab.Panels>
-      </Tab.Group>
+                }
+              >
+                {title}
+              </Tab>
+            ))}
+          </Tab.List>
+          <Tab.Panels>
+            <Tab.Panel>hotels and lodging</Tab.Panel>
+            <Tab.Panel>food</Tab.Panel>
+            <Tab.Panel>transportation</Tab.Panel>
+            <Tab.Panel>places to visit</Tab.Panel>
+          </Tab.Panels>
+        </Tab.Group>
+      </div>
     </>
   );
 };
