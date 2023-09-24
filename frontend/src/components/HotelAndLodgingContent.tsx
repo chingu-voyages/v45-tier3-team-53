f08925d29f888d1a8825fb2642f25a512d6426e3
Kristi <kristiana.d.ingco@gmail.com>
2023-09-24 09:56:46 -0700

    Add cards for hotel in state

--- frontend/src/components/HotelAndLodgingContent.tsx
+++ frontend/src/components/HotelAndLodgingContent.tsx
@@ -1,9 +1,24 @@
+import { FunctionComponent } from "react";
 import { PlaceSearchBar } from "./PlaceSearchBar.tsx";
+import ContentCard from "./ContentCard.tsx";
 
-const HotelAndLodgingContent = () => {
+const HotelAndLodgingContent: FunctionComponent<{
+  hotels: string[];
+  setHotels: (content: string[]) => void;
+}> = ({ hotels, setHotels }) => {
   return (
-    <div>
-      <PlaceSearchBar placeType='lodging' />
+    <div className='flex flex-col'>
+      <PlaceSearchBar
+        placeType='lodging'
+        content={hotels}
+        setContent={setHotels}
+      />
+      <div className='flex flex-wrap overflow-auto h-96'>
+        {hotels &&
+          hotels.map((hotel) => {
+            return <ContentCard title={hotel} />;
+          })}
+      </div>
     </div>
   );
 };
