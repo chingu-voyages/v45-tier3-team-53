5f5dc155fd09e066a356701cddb892b66e82c9ed
Kristi <kristiana.d.ingco@gmail.com>
2023-09-24 08:15:45 -0700

    Make separate component for hotel content

---
+++ frontend/src/components/HotelAndLodgingContent.tsx
@@ -0,0 +1,10 @@
+import { PlaceSearchBar } from "./PlaceSearchBar.tsx";
+
+const HotelAndLodgingContent = () => {
+  return (
+    <div>
+      <PlaceSearchBar placeType='lodging' />
+    </div>
+  );
+};
+export default HotelAndLodgingContent;
