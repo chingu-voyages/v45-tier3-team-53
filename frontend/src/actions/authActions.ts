363e194abfcf7f7b26ede759182dc53e3b5610e5
Chris Yoo <woyoo@umich.edu>
2023-09-28 03:56:53 -0400

    create action for sign up

--- frontend/src/actions/authActions.ts
+++ frontend/src/actions/authActions.ts
@@ -1,1 +1,21 @@
 import * as types from "./types";
+import AuthService from "../services/auth.service";
+
+export const register = (firstName, lastName, email, password) => (dispatch) => {
+    return AuthService.register(firstName, lastName, email, password).then(
+        (response) => {
+            dispatch({
+                type: types.REGISTER_SUCCESS,
+            });
+
+            return Promise.resolve();
+        },
+        (error) => {
+            dispatch({
+                type: types.REGISTER_FAIL,
+            });
+
+            return Promise.reject();
+        }
+    );
+};
