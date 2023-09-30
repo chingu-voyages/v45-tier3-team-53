53221d5174b500817a7e0872adc9f5fa6516b8a6
Shawn Koong <shawnkoong@gmail.com>
2023-09-29 22:36:13 -0700

    testing again

--- frontend/src/pages/Planner.tsx
+++ frontend/src/pages/Planner.tsx
@@ -3,7 +3,7 @@ import { GoogleMapContainer } from '../components/GoogleMapContainer.tsx';
 export const Planner = () => {
   return (
     <div className="grid grid-cols-2">
-      <div>itinerary</div>
+      <div>Itinerary</div>
       <div>
         <GoogleMapContainer />
       </div>
