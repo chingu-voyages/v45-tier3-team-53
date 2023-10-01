5a0abb41159ee45cd74b56751cba0abeaed795af
Chris Yoo <woyoo@umich.edu>
2023-09-30 21:12:42 -0400

    create logout button

--- frontend/src/components/NewTripForm.tsx
+++ frontend/src/components/NewTripForm.tsx
@@ -36,6 +36,13 @@ export const NewTripForm = () => {
   return (
     <>
       <button
+        onClick={() => navigate("/logout")}
+        className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      >
+        Logout
+      </button>
+
+      <button
         onClick={() => navigate("/sign-up")}
         className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
       >
