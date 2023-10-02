71a5f8545962d3ab56d885226b54ff08c98af085
Shawn Koong <shawnkoong@gmail.com>
2023-10-02 15:41:40 -0700

    removing store and provider from App.tsx, since already in main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,6 @@ import { Libraries, useJsApiLoader } from "@react-google-maps/api";
 import { NewTripForm } from "./components/NewTripForm.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Planner } from "./pages/Planner.tsx";
-import { Provider } from "react-redux";
-import { store } from "./store/store.ts";
 import Register from "./components/Register.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./components/Login.tsx";
@@ -19,7 +17,6 @@ function App() {
   localStorage.setItem("isLoaded", String(isLoaded));
 
   return (
-    <Provider store={store}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<NewTripForm />} />
@@ -28,7 +25,6 @@ function App() {
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
-    </Provider>
   );
 }
 
